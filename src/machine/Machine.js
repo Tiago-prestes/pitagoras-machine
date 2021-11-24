1debd63ff52c64ffde2e9f9097958a051bddd69b
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-11-24 20:08:33 -0300

    Designe adicionado

--- src/machine/Machine.js
+++ src/machine/Machine.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import { MainContainer } from './style'
+import { FormContainer, MainContainer, TitleContainer } from './style'
 
 export const Machine = () => {
+    let cateto1
+    let cateto2
+
+    console.log("testes", cateto2)
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <MainContainer>
-            Machine Page pitagoras
-        </MainContainer>
+
+            <FormContainer onSubmit={onSubmitForm}>
+                <TitleContainer>
+                    <h4>Calculadora de Hipotenusa</h4>
+                </TitleContainer>
+                <input
+                    type='text'
+                    placeholder=' Cateto A'
+                    value={cateto1}
+                />
+                <input
+                    type='text'
+                    placeholder=' Cateto B'
+                    value={cateto2}
+                />
+                <input
+                    type='text'
+                    placeholder=' Hipotenusa'
+                />
+                <button>Calcular</button>
+            </FormContainer>
+        </MainContainer >
     )
 }
--- src/machine/style.js
+++ src/machine/style.js
@@ -3,4 +3,38 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
     display: flex;
     justify-content: center;
+    /* border: 1px solid red; */
+    margin-top: 10vh;
+`
+
+export const FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: salmon;
+    border-radius: 8px;
+    width: 25vw;
+    height: 25vw;
+
+        input{
+            height: 30px;
+            width: 15vw;
+            border-radius: 8px;
+            border: none;
+        }
+
+        button{
+            height: 4vh;
+            width: 15vw;
+
+
+            border: none;
+            border-radius: 8px;
+            background-color: pink;
+        }
+`
+
+export const TitleContainer = styled.div`
+
 `

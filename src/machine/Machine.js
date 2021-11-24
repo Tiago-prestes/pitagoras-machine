0d5300b977e4e7932d6b3ae6b114157edbd23272
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-11-24 17:18:19 -0300

    Adicionando o esqueleto

---
+++ src/machine/Machine.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MainContainer } from './style'
+
+export const Machine = () => {
+    return (
+        <MainContainer>
+            Machine Pagede
+        </MainContainer>
+    )
+}

19fbe97e97158a6683a182ea6a0b1e4a53a2e75b
TIAGO PRESTES <83032703+Tiago-prestes@users.noreply.github.com>
2021-11-29 19:10:36 -0300

    Lógica implementada

--- src/machine/style.js
+++ src/machine/style.js
@@ -12,11 +12,11 @@ export const FormContainer = styled.form`
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
-    background-color: salmon;
+    background-color: #2196f3;
     border-radius: 8px;
     width: 25vw;
     height: 25vw;
-
+    box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
         input{
             height: 30px;
             width: 15vw;
@@ -27,14 +27,23 @@ export const FormContainer = styled.form`
         button{
             height: 4vh;
             width: 15vw;
-
-
             border: none;
             border-radius: 8px;
-            background-color: pink;
+            background-color: #6ec6ff; 
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.6);
+            :hover {
+                background-color: #0069c0;
+            }  
+            :active {
+                background-color: #6ec6ff;
+                /* transform: translateY(2px); */
+            }
         }
+
 `
 
 export const TitleContainer = styled.div`
-
+    p{
+        font-weight: bold;
+    }
 `
